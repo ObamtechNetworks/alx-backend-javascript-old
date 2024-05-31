77c824fbc4e6362ffcc48987da4a9fd4b46027fc
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-05-31 02:43:07 +0100

    disable eslint wahala

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
   for (const idx of array) {
+    // eslint-disable-next-line no-param-reassign
     array[i] = appendString + idx;
     i += 1;
   }
