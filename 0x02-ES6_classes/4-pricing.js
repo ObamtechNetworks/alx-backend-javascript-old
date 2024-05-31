c601812b2db26ababd9aaf335d94f65739cdb4c0
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-01 00:54:56 +0100

    task 4 gone

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,61 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    /* eslint-disable no-underscore-dangle */
+    this._amount = this._validateAmount(amount);
+    this._currency = this._validateCurrency(currency);
+  }
+
+  // private validation methods
+  /* eslint-disable class-methods-use-this */
+  // eslint-disable-next-line no-underscore-dangle
+  _validateAmount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a Number');
+    }
+    return amount;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  // eslint-disable-next-line no-underscore-dangle
+  _validateCurrency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    return currency;
+  }
+
+  // Getter and Setter for name
+  get amount() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._amount;
+  }
+
+  set amount(amount) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._amount = this._validateName(amount);
+  }
+
+  get currency() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._currency;
+  }
+
+  set currency(currency) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._currency = this._validateCode(currency);
+  }
+
+  displayFullPrice() {
+    // eslint-disable-next-line no-underscore-dangle
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    const newAmount = this._validateAmount(amount);
+    const newRate = this._validateAmount(conversionRate);
+
+    return newAmount * newRate;
+  }
+}
