93e49547ca7103de0529d911b1d9907eb47a0474
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-06-05 03:33:00 +0100

    fixed bug

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,26 +1,28 @@
 export default function cleanSet(set, startString) {
-  if (Object.prototype.toString.call(set) !== '[object Set]') {
-    throw new TypeError('set must be a Set');
-  }
-
-  if (typeof startString !== 'string') {
-    throw new TypeError('startString must be a String');
-  }
-
-  if (startString.length === 0) {
+// Check if startString is a string and Set is a Set
+  if (typeof startString !== 'string' || !(set instanceof Set) || startString.length === 0) {
+    // If not, return an empty string
     return '';
   }
-  // An empty array to store the cleaned values
+
+  // Create an empty array to store the cleaned values
   const cleanedValues = [];
 
   // Iterate over each value in the set
   set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      // extract the substric into the array and also remove the rest part
+    // Check if the value is a string and starts with the specified startString
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      // If it does, extract substring after the startString and push to the cleanedValues array
       cleanedValues.push(value.slice(startString.length));
     }
   });
 
-  // Join the cleaned values with '-' separator and return as a single string
+  // Check if any values were cleaned and joined
+  if (cleanedValues.length === 0) {
+    // If not, return an empty string
+    return '';
+  }
+
+  // Join the cleaned values with "-" separator and return as a single string
   return cleanedValues.join('-');
 }
